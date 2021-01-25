03559df948d95ae33ed2b7b1235e671a0ddfe151
Babrov <60341182+Babrov@users.noreply.github.com>
2021-01-25 19:43:29 +0200

    add /work page and styles

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
-let items = [];
+
+let items = ['work', 'eat', 'study'];
+let workItemsArr = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   let date = new Date();
@@ -17,12 +22,21 @@ app.get('/', function (req, res) {
   };
   let day = date.toLocaleDateString('en-US', options);
 
-  res.render('list', { day: day, newItem: items });
+  res.render('list', { listTitle: day, newItem: items });
 });
+
 app.post('/', (req, res) => {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect('/');
+  if (req.body.list === 'Work') {
+    workItemsArr.push(item);
+    res.redirect("/work")
+  } else {
+    items.push(item);
+    res.redirect("/")
+  }
+});
+app.get('/work', (req, res) => {
+  res.render('list', { listTitle: 'Work List', newItem: workItemsArr });
 });
 app.listen(3000, function () {
   console.log('Server started on port 3000.');
